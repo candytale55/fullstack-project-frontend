ce31bc49640c1babbea6a2120335db4bf0a7733e
CandyTale55 <candytale55@gmail.com>
2026-09-10 11:46:01 +0200

    LOGINPAGE component with LoginPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import AppLayout from './components/layout/AppLayout/AppLayout.tsx'
-import LoginForm from './components/auth/LoginForm/LoginForm.tsx'
+import LoginPage from './pages/LoginPage/LoginPage.tsx'
 
 export default function App() {
   return (
@@ -7,7 +7,7 @@ export default function App() {
       <AppLayout>
         <h1>Language Learning App</h1>
 
-        <LoginForm />
+        <LoginPage />
 
       </AppLayout>
     </>
