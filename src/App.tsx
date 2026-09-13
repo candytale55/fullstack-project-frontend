4823f85d6711bad671d9dbd1b7b833cfb5950873
CandyTale55 <candytale55@gmail.com>
2026-09-13 19:08:30 +0200

    AUTH - authentication flow with user email display and fallback, and update types for clarity - Simple test to check the form is working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import AppLayout from './components/layout/AppLayout/AppLayout.tsx'
 import LoginPage from './pages/LoginPage/LoginPage.tsx'
+import Button from './components/ui/Button/Button.tsx'
+
+import  useAuth  from './hooks/useAuth.ts'
 
 export default function App() {
+
+  const { user, isAuthenticated, logout } = useAuth()
+
   return (
     <>
       <AppLayout>
         <h1>Language Learning App</h1>
+        {isAuthenticated && user ? (
+          <>
+            <p>Welcome, {user.name}</p>
+            <p>{user.email}</p>
 
-        <LoginPage />
-
+            <Button onClick={logout}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <LoginPage />
+        )}
       </AppLayout>
     </>
   )
