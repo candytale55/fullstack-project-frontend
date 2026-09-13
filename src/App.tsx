8763eec3ca1759ea2b67a0b307a1d17249ee7842
CandyTale55 <candytale55@gmail.com>
2026-09-13 21:21:59 +0200

    DASHBOARD feat: Add basic Dashboard page and styles, add routing to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
+import { Routes, Route } from 'react-router'
 import AppLayout from './components/layout/AppLayout/AppLayout.tsx'
+import DashboardPage from './pages/DashboardPage/DashboardPage.tsx'
 import LoginPage from './pages/LoginPage/LoginPage.tsx'
-import Button from './components/ui/Button/Button.tsx'
-
-import  useAuth  from './hooks/useAuth.ts'
 
 export default function App() {
-
-  const { user, isAuthenticated, logout } = useAuth()
 
   return (
     <>
       <AppLayout>
-        <h1>Language Learning App</h1>
-        {isAuthenticated && user ? (
-          <>
-            <p>Welcome, {user.name}</p>
-            <p>{user.email}</p>
-
-            <Button onClick={logout}>
-              Logout
-            </Button>
-          </>
-        ) : (
-          <LoginPage />
-        )}
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
       </AppLayout>
     </>
   )
---
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -0,0 +1,10 @@
+import styles from './DashBoardPage.module.css'
+
+export default function DashboardPage() { 
+    return (
+        <div className={styles.dashboard}>
+            <h1>Dashboard</h1>
+            <p>Welcome to the dashboard!</p>
+        </div>
+    )
+}
