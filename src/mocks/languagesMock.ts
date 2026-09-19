f96998ed01d24295f78de68a8f948fec5c523e1d
CandyTale55 <candytale55@gmail.com>
2026-09-19 13:42:05 +0200

    LANGUAGE - refactor: update languagesMock to include nativeName property for all languages (Necessary to avoid conflict with new type)

--- src/mocks/languagesMock.ts
+++ src/mocks/languagesMock.ts
@@ -1,16 +1,30 @@
+/*
+ * src/mocks/languagesMock.ts
+ *
+ * Temporary language data used by pages that have not yet been connected
+ * to the backend. (Changed to include nativeName property)
+ */
+
 import type { Language } from '../types/language'
+
 
 export const languagesMock: Language[] = [
     {
-        id: 'portuguese',
-        name: 'Portugués',
-        code: 'pt',
-        availableCourses: 3,
-    },
-    {
         id: 'english',
         name: 'Inglés',
+        nativeName: 'English',
         code: 'en',
-        availableCourses: 2,
+    },
+    {
+        id: 'portuguese',
+        name: 'Portugués',
+        nativeName: 'Português',
+        code: 'pt',
+    },
+    {
+        id: 'french',
+        name: 'Francés',
+        nativeName: 'Français',
+        code: 'fr',
     },
 ]
